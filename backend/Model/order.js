4278df694ba15870ce9d98205d34603e60f10993
Amanuel Wakshum <amanuelwakshum@gmail.com>
2021-08-15 16:49:34 +0300

    created order schema

---
+++ backend/Model/order.js
@@ -0,0 +1,104 @@
+const mongoose = require('mongoose')
+
+const OrderSchema = mongoose.Schema({
+
+    shippingInfo :{
+        address: {
+            type: String,
+            required: true
+        },
+         city: {
+            type: String,
+            required: true
+        },
+         phoneNo: {
+            type: String,
+            required: true
+        },
+         postalCode: {
+            type: String,
+            required: true
+        },
+         country: {
+            type: String,
+            required: true
+        }
+    },
+    user: {
+       type: mongoose.Schema.Types.ObjectId,
+       required: true,
+       ref: 'User'
+    },
+    orderItems:[
+        {
+           name: {
+            type: String,
+            required: true
+        },
+         quantity: {
+            type: Number,
+            required: true
+        },
+        image:{
+            type: String,
+            required: true
+        },
+        price: {
+            type: Number,
+            required: true
+        },
+        product: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'Product'
+        }
+        }
+    ],
+    paymentInfo: {
+        id: {
+            type: String,
+            
+        },
+        status: {
+            type: String,
+          
+        }
+    },
+    piadAt: {
+     type: Date
+    },
+    itemsPrice: {
+        type: Number,
+        required: true,
+        default: 0.0
+    },
+    shippingPrice: {
+         type: Number,
+        required: true,
+        default: 0.0
+    },
+    taxPrice: {
+        type: Number,
+        required: true,
+        default: 0.0
+    },
+    totalPrice: {
+        type: Number,
+        required: true,
+        default: 0.0
+    },
+    orderStatus: {
+        type: String,
+        required: true,
+        default: 'processing'
+    },
+    deliveredAt:{
+        type: Date
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = mongoose.model('Order', OrderSchema)
